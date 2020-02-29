5b819dff7a2d7e9289f127a076d87c6a22207956
Martin Fleurial <martinfl@pop-os.localdomain>
2020-02-29 16:30:54 +0100

    Amélioration de sum. Première version de prod (à améliorer maybe?) Écriture d'une procédure write_digits pour afficher l'écriture des chiffres en base 2^32

--- code/utils.pas
+++ code/utils.pas
@@ -10,13 +10,29 @@ const BASE: QWord = 1 shl 32;
 /// ex: t = [5, 3] <=> t = 5 * (2^32)^0 + 3 * (2^32)^1 = 12884901893
 type int_array = array of LongWord;
 
+procedure write_digits(a: int_array);
+
 procedure normalize (var t: int_array);
 function smaller (a, b: int_array) : Boolean;
 
 procedure sum(a, b: int_array; var s: int_array);
 procedure diff (a, b: int_array; var s: int_array);
 
+procedure prod (a, b : int_array; var p : int_array);
+
 implementation
+
+/// Affiche l'écriture en base 2^32 du int_array,
+/// avec le chiffre de poids faible à gauche
+procedure write_digits(a: int_array);
+var i:LongWord;
+begin
+  write ('[');
+  for i:=0 to high (a) - 1 do
+    write (a[i], ', ');
+  writeln (a[high (a)], ']');
+end;
+
 
 /// Si le chiffre de poids fort est nul, alors il est retiré.
 /// ex:
@@ -27,7 +43,10 @@ implementation
 /// ```
 procedure normalize (var t : int_array);
 begin
-    while t[length(t) - 1] = 0 do
+	// On doit vérifier que t <> nil en premier
+	// car si t = nil, alors length (t) n'existe pas
+	// et le programme s'arrête.
+    while (t <> nil) and (t[length(t) - 1] = 0)do
         SetLength (t, length(t) - 1);
 end;
 
@@ -59,52 +78,45 @@ begin
 end;
 
 /// Réalise la somme de deux int_array
-procedure sum(a, b: int_array; var s: int_array);
+procedure sum (a, b: int_array; var s: int_array);
 var i: LongWord;
 	somme: QWord;
 	retenue: LongWord;
-	max, min: int_array;
 begin
 	Normalize (a); Normalize (b);
-	if smaller(a, b) then
+
+	if smaller (a, b) then
+		sum (b, a, s)
+	else 
 	begin
-		min := a;
-		max := b;
-	end
-	else
-	begin
-		min := b;
-		max := a;
+		setLength (s, length (a) + 1);
+
+		retenue := 0;
+		for i:=0 to high (s) do
+		begin
+			// On utilise un QWord dans le cas où la somme est plus
+			// grande que la base pour pouvoir calculer la retenue
+			if i <= High (b) then
+				somme := a[i] + retenue + b[i]
+			else
+				somme := a[i] + retenue;
+
+			retenue := 0;
+			if somme >= BASE then 
+				retenue := somme div BASE;
+
+			s[i] := somme mod BASE;
+		end;
+		Normalize (s);
 	end;
-
-	// On a length (s) = max (length (a), length (b)) + 1
-	setLength (s, length(max) + 1);
-	s := copy (max, 0, length (max));
-
-	retenue := 0;
-
-	// On stocke temporairement le résultat de la somme
-	// dans un QWord en cas de dépassement, pour pouvoir
-	// calculer la retenue dans tous les cas et la propager.
-	for i:=0 to high (min) do
-	begin
-		somme := s[i] + min[i] + retenue;				
-		retenue := somme div BASE;
-		// Note: ici l'expression est équivalente à s[i] := somme mod BASE.
-		s[i] := somme;
-	end;
-
-	Normalize (s);
 end;
 
 /// Réalise la soustraction de deux int_array
-/// Note: ici on impose a >= b;
+/// Note: ici on suppose a >= b;
 procedure diff (a, b: int_array; var s: int_array);
 var i: LongWord;
-	difference: Int64;
-	retenue: Int64;
+	difference, retenue: Int64;
 begin
-	assert (not (smaller (a, b)));
 	Normalize (a); Normalize (b);
 	
 	setLength (s, length (a));
@@ -112,8 +124,8 @@ begin
 	retenue := 0;
 	for i:=0 to High (s) do
 	begin
-		// On utilise un Int64 dans le cas où la différence est négative
-		// pour pouvoir calculer la retenue
+		// On utilise un Int64 dans le cas où la différence
+		// est négative pour pouvoir calculer la retenue
 		if i <= High (b) then
 			difference := a[i] + retenue - b[i]
 		else
@@ -125,14 +137,51 @@ begin
 		// la base et retirer 1 au quotient.
 		if difference < 0 then 
 			retenue := (difference div BASE) - 1;
-		// Note: ici l'expression est équivalente à s[i] := difference mod BASE.
-		s[i] := difference;
+
+		s[i] := difference mod BASE;
 	end;
 
-	Normalize (s)
+	Normalize (s);
 end;
 
+procedure prod (a, b : int_array; var p : int_array);
+var n, m: Longword;
+	produit: QWord;
+	retenue, retenue_ld, bs_atteinte: LongWord;
+begin
+	Normalize (a); Normalize (b);
+	setLength (p, length (a) + length (b) + 1);
 
+	// ça marche mais à refaire?
+	// explication :
+	// la somme n+m n'est pas croissante dans la boucle ci-dessous,
+	// donc il faut conserver la retenue quand on retourne sur une
+	// valeur inférieure de n+m, et l'ajouter quand atteint n+m+1
+	// de plus, quand on atteint high(a)+high(b) il faut déplacer
+	// la retenue à high(a)+high(b)+1
+	retenue_ld := 0;
+	bs_atteinte := 0;
+	for n:=0 to high (a) do
+	begin
+		retenue := 0;
+		for m:=0 to high (b) do
+		begin
+			produit := a[n]*b[m] + retenue;
+			if (n + m = bs_atteinte + 1) then
+				produit := produit + retenue_ld;
+			retenue := produit div BASE;
+			p[n+m] := (p[n+m] + produit) mod BASE;
+			if m = high (b) then
+			begin
+			    retenue_ld := retenue;
+				bs_atteinte := n + m; 
+			end;
+			if (n = high (a)) and (m = high (b)) then
+				p[n+m+1] := retenue;
+		end;
+	end;
 
+	Normalize (p);
+end;
 end.
 
